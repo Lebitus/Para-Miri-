72cc0d7642f9fd5cbc5743b7f71209b2155c54f8
Lebitus <deivyherrera630@gmail.com>
2025-07-29 00:10:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-script.js
+function mostrarMensaje() {
+  alert("Te amo Miri ❤️");
+}
+
+function crearCorazon() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+  heart.innerText = "❤️";
+  document.body.appendChild(heart);
+  setTimeout(() => {
+    heart.remove();
+  }, 6000);
+}
+
+setInterval(crearCorazon, 500);
